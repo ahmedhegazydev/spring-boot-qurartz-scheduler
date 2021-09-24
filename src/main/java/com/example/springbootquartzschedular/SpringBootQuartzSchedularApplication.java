c8511ca1991f2a36b48de9bde93603d24f62a90a
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-09-25 00:08:45 +0200

    init commit

---
+++ src/main/java/com/example/springbootquartzschedular/SpringBootQuartzSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootquartzschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzSchedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzSchedularApplication.class, args);
+    }
+
+}
